136011b7ed3d7543aac8e4f643593153614ec62f
iivanov99 <ivko3run@gmail.com>
2019-11-29 22:30:21 +0200

    LoginForm refactored with hooks

--- softuni-project/project-front-end/src/components/Login/LoginForm/LoginForm.jsx
+++ softuni-project/project-front-end/src/components/Login/LoginForm/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import * as yup from 'yup';
 import userService from '../../../services/user-service';
@@ -14,32 +14,20 @@ const schema = yup.object().shape({
     .max(12, 'Password must not exceed 12 symbols!')
 });
 
-class LoginForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: ''
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
-  }
+const LoginForm = ({ history, changeLoggedState }) => {
 
-  handleInputChange(ev) {
-    this.setState({
-      [ev.target.name]: ev.target.value
-    });
-  }
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  async handleFormSubmit(ev) {
+  const handleFormSubmit = async (ev) => {
     ev.preventDefault();
 
     try {
-      await schema.validate(this.state, { abortEarly: false });
-      await userService.login(this.state);
+      await schema.validate({ email, password }, { abortEarly: false });
+      await userService.login({ email, password });
 
-      this.props.changeLoggedState(true);
-      this.props.history.push('/');
+      changeLoggedState(true);
+      history.push('/');
 
       toast.dismiss();
       toast.success('Logged in successfuly!');
@@ -55,31 +43,27 @@ class LoginForm extends Component {
         toast.error('Invalid email or password!');
       }
     }
-  }
+  };
 
-  render() {
-    const { email, password } = this.state;
-
-    return (
-      <div className="row row-dark-grey row-login">
-        <div className="col-md-12">
-          <form onSubmit={this.handleFormSubmit} className="login-form">
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input onChange={this.handleInputChange} className="form-control" value={email}
-                type="text" name="email" id="email" placeholder="Email" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input onChange={this.handleInputChange} className="form-control" value={password}
-                type="password" name="password" id="password" placeholder="Password" />
-            </div>
-            <button type="submit" className="btn btn-primary form-btn">Login</button>
-          </form>
-        </div>
+  return (
+    <div className="row row-dark-grey row-login">
+      <div className="col-md-12">
+        <form onSubmit={handleFormSubmit} className="login-form">
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input onChange={ev => setEmail(ev.target.value)} value={email}
+              className="form-control" type="text" id="email" placeholder="Email" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input onChange={ev => setPassword(ev.target.value)} value={password}
+              className="form-control" type="password" id="password" placeholder="Password" />
+          </div>
+          <button type="submit" className="btn btn-primary form-btn">Login</button>
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default LoginForm;
