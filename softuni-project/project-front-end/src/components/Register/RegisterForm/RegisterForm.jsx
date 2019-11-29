108c7a65b39d1e93f4dd267c4efe9aa426641045
iivanov99 <ivko3run@gmail.com>
2019-11-29 22:10:19 +0200

    RegisterForm comp refactored with hooks

--- softuni-project/project-front-end/src/components/Register/RegisterForm/RegisterForm.jsx
+++ softuni-project/project-front-end/src/components/Register/RegisterForm/RegisterForm.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { toast } from 'react-toastify';
 import * as yup from 'yup';
 import userService from '../../../services/user-service';
@@ -21,33 +21,20 @@ const schema = yup.object().shape({
     .required('Repeat password is required!')
 });
 
-class RegisterForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      username: '',
-      password: '',
-      rePassword: ''
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
-  }
+const RegisterForm = ({ history }) => {
 
-  handleInputChange(ev) {
-    this.setState({
-      [ev.target.name]: ev.target.value
-    });
-  }
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [rePassword, setRePassword] = useState('');
 
-  async handleFormSubmit(ev) {
+  const handleFormSubmit = async (ev) => {
     ev.preventDefault();
 
     try {
-      await schema.validate(this.state, { abortEarly: false });
-      await userService.register(this.state);
-
-      this.props.history.push('/user/login');
+      await schema.validate({ email, username, password, rePassword }, { abortEarly: false });
+      await userService.register({ email, username, password, rePassword });
+      history.push('/user/login');
       toast.dismiss();
       toast.success('Registered successfuly!');
     } catch (err) {
@@ -60,45 +47,40 @@ class RegisterForm extends Component {
 
       toast.error(err.message);
     }
-  }
+  };
 
-
-  render() {
-    const { email, username, password, rePassword } = this.state;
-
-    return (
-      <div className="row row-dark-grey row-register">
-        <div className="col-md-12">
-          <form onSubmit={this.handleFormSubmit} className="register-form">
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input onChange={this.handleInputChange} className="form-control" value={email}
-                type="text" name="email" id="email" placeholder="Email" />
-              <small id="emailHelp" className="form-text text-muted">
-                We'll never share your email adress with anyone else.
-              </small>
-            </div>
-            <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <input onChange={this.handleInputChange} className="form-control" value={username}
-                type="text" name="username" id="username" placeholder="Username" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input onChange={this.handleInputChange} className="form-control" value={password}
-                type="password" name="password" id="password" placeholder="Password" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="rePassword">Repeat Password</label>
-              <input onChange={this.handleInputChange} className="form-control" value={rePassword}
-                type="password" name="rePassword" id="rePassword" placeholder="Password" />
-            </div>
-            <button type="submit" className="btn btn-primary form-btn">Register</button>
-          </form>
-        </div>
+  return (
+    <div className="row row-dark-grey row-register">
+      <div className="col-md-12">
+        <form onSubmit={handleFormSubmit} className="register-form">
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input onChange={(ev) => setEmail(ev.target.value)} value={email}
+              className="form-control" type="text" id="email" placeholder="Email" />
+            <small id="emailHelp" className="form-text text-muted">
+              We'll never share your email adress with anyone else.
+            </small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input onChange={(ev) => setUsername(ev.target.value)} value={username}
+              className="form-control" type="text" id="username" placeholder="Username" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input onChange={(ev) => setPassword(ev.target.value)} value={password}
+              className="form-control" type="password" id="password" placeholder="Password" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="rePassword">Repeat Password</label>
+            <input onChange={(ev) => setRePassword(ev.target.value)} value={rePassword}
+              className="form-control" type="password" id="rePassword" placeholder="Password" />
+          </div>
+          <button type="submit" className="btn btn-primary form-btn">Register</button>
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default RegisterForm;
