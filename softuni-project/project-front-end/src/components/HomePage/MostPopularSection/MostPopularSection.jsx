19bfe438fe4b75c468d854226b5c93c61758cc63
iivanov99 <ivko3run@gmail.com>
2019-11-30 07:00:34 +0200

    MostPopularSection component refactored with hooks

--- softuni-project/project-front-end/src/components/HomePage/MostPopularSection/MostPopularSection.jsx
+++ softuni-project/project-front-end/src/components/HomePage/MostPopularSection/MostPopularSection.jsx
@@ -1,48 +1,48 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment, Component, useState, useEffect } from 'react';
 import ApparelHeading from '../ApparelHeading/ApparelHeading';
 import ProductList from '../../Products/ProductList/ProductList';
 import apparelService from '../../../services/apparel-service';
 
-class MostPopularSection extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      menApparel: [],
-      womenApparel: [],
-      accessories: []
-    };
-  }
+const MostPopularSection = () => {
 
-  async componentDidMount() {
-    this.setState({
-      menApparel: await apparelService.loadAll('men', 4),
-      womenApparel: await apparelService.loadAll('women', 4),
-      accessories: await apparelService.loadAll('accessories', 4)
-    });
-  }
+  const [menApparel, setMenApparel] = useState([]);
+  const [womenApparel, setWomenApparel] = useState([]);
+  const [accessories, setAccessories] = useState([]);
 
-  render() {
-    const { menApparel, womenApparel, accessories } = this.state;
+  const fetchApparel = async () => {
+    const [men, women, acc] = await Promise.all([
+      apparelService.loadAll('men', 4),
+      apparelService.loadAll('women', 4),
+      apparelService.loadAll('accessories', 4)
+    ]);
 
-    return (
-      <Fragment>
-        <div className="row row-silver">
-          <div className="col-md-12 text-center">
-            <h1>Most <span className="underline">Popular</span></h1>
-          </div>
+    setMenApparel(men);
+    setWomenApparel(women);
+    setAccessories(acc);
+  };
+
+  useEffect(() => {
+    fetchApparel();
+  }, []);
+
+  return (
+    <Fragment>
+      <div className="row row-silver">
+        <div className="col-md-12 text-center">
+          <h1>Most <span className="underline">Popular</span></h1>
         </div>
+      </div>
 
-        <ApparelHeading name="Men Apparel" />
-        <ProductList apparel={menApparel} apparelType="men"/>
+      <ApparelHeading name="Men Apparel" />
+      <ProductList apparel={menApparel} apparelType="men" />
 
-        <ApparelHeading name="Women Apparel" />
-        <ProductList apparel={womenApparel} apparelType="women"/>
+      <ApparelHeading name="Women Apparel" />
+      <ProductList apparel={womenApparel} apparelType="women" />
 
-        <ApparelHeading name="Accessories" />
-        <ProductList apparel={accessories} apparelType="accessories"/>
-      </Fragment>
-    );
-  }
-}
+      <ApparelHeading name="Accessories" />
+      <ProductList apparel={accessories} apparelType="accessories" />
+    </Fragment>
+  );
+};
 
 export default MostPopularSection;
