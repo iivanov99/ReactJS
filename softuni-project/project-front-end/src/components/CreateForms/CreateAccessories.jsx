645648ece5f634ba9ce8894cb9392a6ae5aeede2
iivanov99 <ivko3run@gmail.com>
2019-12-02 17:30:31 +0200

    Create Accessories form added

--- softuni-project/project-front-end/src/components/CreateForms/CreateAccessories.jsx
+++ softuni-project/project-front-end/src/components/CreateForms/CreateAccessories.jsx
@@ -1,60 +1,15 @@
 import React, { Fragment } from 'react';
 import CreateFormHeading from './CreateFormHeading/CreateFormHeading';
+import CreateForm from './CreateForm/CreateForm';
 
-const CreateAccessories = () => {
+const CreateMens = () => {
   return (
     <Fragment>
       <CreateFormHeading formName="Accessories" />
-      <div className="row row-dark-grey">
-        <div className="col-md-12">
-          <form className="create-form">
-            <div className="form-group">
-              <label htmlFor="name">Name</label>
-              <input type="text" className="form-control" id="name" placeholder="Name" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="description">Description</label>
-              <textarea className="form-control" placeholder="Description"></textarea>
-            </div>
-            <div className="form-group">
-              <label htmlFor="imageUrl">Image Url</label>
-              <input type="text" className="form-control" id="imageUrl" placeholder="ImageUrl" />
-            </div>
-            <div className="form-group flex-container">
-              <div>
-                <label htmlFor="size">Size: </label>
-                <select className="form-control size-select">
-                  <option>XS</option>
-                  <option>S</option>
-                  <option>M</option>
-                  <option>L</option>
-                  <option>XL</option>
-                  <option>No Size</option>
-                </select>
-              </div>
-              <div>
-                <label htmlFor="category">Category: </label>
-                <select defaultValue="Choose category" className="form-control select-control">
-                  <option disabled>Choose category</option>
-                  <option>Hat</option>
-                  <option>Workout Belt</option>
-                  <option>Resistence Band</option>
-                  <option>Wristbands</option>
-                  <option>Gloves</option>
-                  <option>Other</option>
-                </select>
-              </div>
-              <div className="price-container">
-                <label htmlFor="category">Price: </label>
-                <input className="form-control" type="number" placeholder="Price" min="1" />
-              </div>
-            </div>
-            <button type="submit" className="btn btn-primary form-btn create-btn">Create</button>
-          </form>
-        </div>
-      </div>
+      <CreateForm formName="Accessories"
+        categoryOptions={['Hat', 'Workout Belt', 'Resistence Bands', 'Wristbands', 'Gloves', 'Other']} />
     </Fragment>
-  )
+  );
 };
 
-export default CreateAccessories;
+export default CreateMens;
