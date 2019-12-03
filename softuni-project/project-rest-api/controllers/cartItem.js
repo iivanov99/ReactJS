13b59174b8413d82477b391324a94ec3b76f9fe3
iivanov99 <ivko3run@gmail.com>
2019-12-03 16:00:12 +0200

    rest-api delete cartItem also removes it from current user's cart items array

--- softuni-project/project-rest-api/controllers/cartItem.js
+++ softuni-project/project-rest-api/controllers/cartItem.js
@@ -19,15 +19,26 @@ module.exports = {
       }
     }
   },
-  post: async (req, res, next) => {
-    try {
-      const { user } = req;
-      const { name, price } = req.body;
-      const createdCartItem = await models.CartItem.create({ name, price, creatorId: user._id });
-      await models.User.updateOne({ _id: user._id }, { $push: { cartItems: createdCartItem._id } });
-      res.json(createdCartItem);
-    } catch (err) {
-      next(err);
+  post: {
+    createOne: async (req, res, next) => {
+      try {
+        const { user } = req;
+        const { name, price } = req.body;
+        const createdCartItem = await models.CartItem.create({ name, price, creatorId: user._id });
+        await models.User.updateOne({ _id: user._id }, { $push: { cartItems: createdCartItem._id } });
+        res.json(createdCartItem);
+      } catch (err) {
+        next(err);
+      }
+    },
+    checkout: async (req, res, next) => {
+      try {
+        const { user } = req;
+        console.log(user);
+
+      } catch (err) {
+
+      }
     }
   },
   put: async (req, res, next) => {
@@ -40,13 +51,29 @@ module.exports = {
       next(err);
     }
   },
-  delete: async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const deleteInfo = await models.CartItem.deleteOne({ _id: id });
-      res.json(deleteInfo);
-    } catch (err) {
-      next(err);
+  delete: {
+    all: async (req, res, next) => {
+      try {
+        const { user } = req;
+        const deleteInfo = await models.CartItem.deleteMany({ creatorId: user._id });
+        await models.User.updateOne({ _id: user._id }, { $set: { cartItems: [] } });
+        res.json(deleteInfo);
+      } catch (err) {
+        next(err);
+      }
+    },
+    one: async (req, res, next) => {
+      try {
+        const { user } = req;
+        const { id } = req.params;
+        const [deleteInfo, updateInfo] = await Promise.all([
+          models.CartItem.deleteOne({ _id: id }),
+          models.User.updateOne({ _id: user._id }, { $pull: { cartItems: id } }),
+        ]);
+        res.json(deleteInfo);
+      } catch (err) {
+        next(err);
+      }
     }
   }
 };
